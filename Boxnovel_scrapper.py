a33c2c0c6df59ae1126a04f1e9e5fa40b876fe01
vallabh1822 <71896374+vallabh1822@users.noreply.github.com>
2022-03-19 15:21:37 +0530

    Add files via upload

---
+++ Boxnovel_scrapper.py
@@ -0,0 +1,80 @@
+"""
+To scrape novel chapter from 'Boxnovel.com' and convert it in epub formats (create epub file) and return its reference
+functions in module:
+1.convertToEpub(doc:html, nameOfNovel)
+2.BoxNovelScrape(StartChapter:int =1, EndChapter:int =2, name="ANOTHER WORLD’S VERSATILE CRAFTING MASTER"):
+"""
+
+
+import random
+from bs4 import BeautifulSoup, Doctype
+import requests
+import pypandoc
+import re
+import time
+
+
+
+
+
+def convertToEpub(doc,nameOfNovel):
+    
+     
+    htmlName= nameOfNovel + ".html"
+    with open(htmlName, "w",encoding='utf-8') as file:
+        file.write(str(doc))
+    epubName= nameOfNovel + ".epub"
+    
+
+    
+    output= pypandoc.convert_file(htmlName,'epub3', outputfile=epubName)  
+    file.close()
+    return output
+
+
+
+def BoxNovelScrape(StartChapter:int =1, EndChapter:int =2, name="ANOTHER WORLD’S VERSATILE CRAFTING MASTER"):
+    doc = BeautifulSoup()
+
+    doc.append(Doctype('html'))
+    html = doc.new_tag('html', lang='en-US')
+    doc.append(html)
+    head = doc.new_tag('head')
+    html.append(head)
+    meta = doc.new_tag('meta', charset='utf-8')
+    head.append(meta)
+    title = doc.new_tag('title')
+    title.string = name
+    head.append(title)
+    body = doc.new_tag('body')
+    html.append(body)
+
+    nameOfNovel =name
+    nameOfNovel =nameOfNovel.lower()
+    nameOfNovel= re.sub(r'[^\w\s]', '', nameOfNovel)
+    nameOfNovel = nameOfNovel.replace(" ", "-")
+
+
+
+    for i in range (StartChapter, EndChapter):
+        url ="https://boxnovel.com/novel/"+nameOfNovel+"/chapter-{}/".format(i)
+        r1= requests.get(url)
+        time.sleep(random.randint(2,11))
+        chapter=r1.content
+        soup1 = BeautifulSoup(chapter, 'html5lib')
+        chapter_con = soup1.find_all('p')
+
+        header = doc.new_tag('h1')
+        header.string = "chapter {}".format(i)
+        body.append(header)
+
+
+        # Append content to html
+        for i, child in enumerate(chapter_con):
+                if child==chapter_con[-21]:
+                    break
+                body.append(child)
+    
+    name = name + " {}-{}".format(StartChapter, EndChapter)
+    output = convertToEpub(doc, name)
+    return output    
